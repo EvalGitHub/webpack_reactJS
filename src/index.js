d45b3f4ded81a9d65bde328ca956899bb1a1fa34
yirui <yirui@yiruideMacBook-Pro.local>
2019-07-11 19:21:57 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+     <div>this is demo</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
