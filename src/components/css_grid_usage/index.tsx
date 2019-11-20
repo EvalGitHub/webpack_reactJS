50562b5df32ad481a4266076e84334a0b83f27f8
yirui <yirui@codemao.cn>
2019-11-20 18:40:16 +0800

    feat:添加css-grid的使用案例

--- src/components/css_grid_usage/index.tsx
+++ src/components/css_grid_usage/index.tsx
@@ -3,12 +3,20 @@ import * as styles from './style.scss';
 function CssGridUsage () {
   return (
     <div className={styles.css_grid_wrapper}>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
+        <div className={[`${styles.itmes}`, `${styles.item1}`].join(' ')}>1</div>
+        <div className={styles.itmes}>2</div>
+        <div className={styles.itmes}>3</div>
+        <div className={styles.itmes}>4</div>
+        <div className={styles.itmes}>5</div>
+        <div className={styles.itmes}>6</div>
+        <div className={styles.itmes}>5</div>
+        <div className={styles.itmes}>6</div>
+        <div className={styles.itmes}>5</div>
+        <div className={styles.itmes}>6</div>
+        <div className={styles.itmes}>5</div>
+        <div className={styles.itmes}>6</div>
+        <div className={styles.itmes}>5</div>
+        <div className={styles.itmes}>6</div>
     </div>
   );
 }
