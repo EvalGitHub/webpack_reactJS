4b07cfbba14ab1160d1cffbe340784eaf5016914
yirui <yirui@codemao.cn>
2019-12-26 18:36:30 +0800

    perf:默认路由跳转

--- src/route/index.js
+++ src/route/index.js
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import * as React from 'react';
 import loadable from "@loadable/component";
 // route to component 
@@ -6,14 +6,17 @@ function Routeconfig() {
   return (
     <Router>
       {/* <RouteLink/> */}
-      <Route exact path="/" component={loadable(() => import("@/components/login"))}/>
-      <Route exact path="/detail" component={loadable(() => import("@/components/detail"))}/>
-      <Route exact path="/hook_component" component={loadable(() => import("@/components/hook_component"))}/>
-      <Route exact path="/create_ele" component={loadable(() => import("@/components/react_create_ele"))}/>
-      <Route exact path="/event_trans_info" component={loadable(() => import("@/components/event_trans_info"))}/>
-      <Route exact path="/css_grid_usage" component={loadable(() => import("@/components/css_grid_usage"))}/>
-      <Route exact path="/hight_component" component={loadable(() => import("@/components/hight_component/my_component"))}/>
-      <Route exact path="/hight_component02" component={loadable(() => import("@/components/hight_component/my_component02"))}/>
+      <Switch>
+        <Route exact path="/" component={loadable(() => import("@/components/login"))}/>
+        <Route exact path="/detail" component={loadable(() => import("@/components/detail"))}/>
+        <Route exact path="/hook_component" component={loadable(() => import("@/components/hook_component"))}/>
+        <Route exact path="/create_ele" component={loadable(() => import("@/components/react_create_ele"))}/>
+        <Route exact path="/event_trans_info" component={loadable(() => import("@/components/event_trans_info"))}/>
+        <Route exact path="/css_grid_usage" component={loadable(() => import("@/components/css_grid_usage"))}/>
+        <Route exact path="/hight_component" component={loadable(() => import("@/components/hight_component/my_component"))}/>
+        <Route exact path="/hight_component02" component={loadable(() => import("@/components/hight_component/my_component02"))}/>
+        <Redirect exact from="/" to="/detail"></Redirect>
+      </Switch>
     </Router>
   )
 }
