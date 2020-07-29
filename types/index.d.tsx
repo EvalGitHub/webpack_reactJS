d094703c68776819d14aba251114088af9a12282
EvalGitHub <957010831@qq.com>
2020-07-29 11:05:09 +0800

    feat：index.d.tsx

---
+++ types/index.d.tsx
@@ -0,0 +1,17 @@
+declare module 'is_js';
+declare module 'jquery';
+declare module 'lodash-es';
+declare module 'weixin-js-sdk';
+declare module '@mlz/perf-report';
+declare module '*';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+interface Window {
+  QC:any;
+  jQuery:any;
+}
