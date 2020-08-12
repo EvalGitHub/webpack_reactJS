a6d5e9ff236aca20eb3b18f0c08723c8adac5bc8
EvalGitHub <957010831@qq.com>
2020-08-12 19:33:47 +0800

    feat：img_minify_webpack_plugin

--- my_plugins/img_minify_webpack_plugin/comprocess_img.js
+++ my_plugins/img_minify_webpack_plugin/comprocess_img.js
@@ -10,13 +10,17 @@ const { UploadImg, DownloadImg } = require('./down_up_load_img');
 async function CompressImg(assets, path) {
   try {
     const file = assets[path].source(); // 获取图片资源
-    const obj = await UploadImg(file);
-    const data = await DownloadImg(obj.output.url);
-    console.log('obj......', obj);
+    const obj = await UploadImg(file); // 返回文件的信息{size, type, width, height, ratio, url, }
     const oldSize = Chalk.redBright(ByteSize(obj.input.size));
     const newSize = Chalk.greenBright(ByteSize(obj.output.size));
     const ratio = Chalk.blueBright(RoundNum(1 - obj.output.ratio, 2, true));
+
+    const data = await DownloadImg(obj.output.url); 
+
+
     const dpath = assets[path].existsAt;
+    // 判断文件嘉是否存在
+
     const msg = `${Figures.tick} Compressed [${Chalk.yellowBright(path)}] completed: Old Size ${oldSize}, New Size ${newSize}, Optimization Ratio ${ratio}`;
     Fs.writeFileSync(dpath, data, "binary");
     return Promise.resolve(msg);
